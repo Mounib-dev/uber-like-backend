b8ea1fd5d76c9176895e68b2c6b4c022c2cd2f2f
OUROUA M. Mounib <ourouamounib@gmail.com>
2025-04-29 11:37:02 +0200

    Client Service: adding register, login and user info routes

--- client-ms-api-uber-like/src/app.ts
+++ client-ms-api-uber-like/src/app.ts
@@ -6,7 +6,13 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cors from "cors";
 
+import { AppDataSource } from "./data-source";
+
 import api from "./routes";
+
+import loginRoute from "./routes/auth/login.route";
+import registerRoutes from "./routes/auth/register.route";
+import userRoute from "./routes/auth/profile.route";
 
 const app = express();
 
@@ -15,7 +21,18 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+AppDataSource.initialize()
+  .then(() => {
+    console.log("🛢️  Connected To Database Client");
+  })
+  .catch((err) => {
+    console.error(err);
+    console.log("⚠️ Error to connect Database Client");
+  });
+
 app.use("/", api);
+app.use("/api/v1/auth", loginRoute);
+app.use("/api/v1/user", registerRoutes, userRoute);
 
 const port = process.env.PORT || 3010;
 
