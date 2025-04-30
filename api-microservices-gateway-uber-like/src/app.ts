b6940f2669ed062f41c6f47afcf2d0b574e375b3
OUROUA M. Mounib <ourouamounib@gmail.com>
2025-04-30 14:51:17 +0200

    Adding sockets to backend and configuration

--- api-microservices-gateway-uber-like/src/app.ts
+++ api-microservices-gateway-uber-like/src/app.ts
@@ -1,7 +1,11 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import express, { RequestHandler } from "express";
+import express, { NextFunction, Request, Response } from "express";
+
+import { createServer } from "http";
+import { Server } from "socket.io";
+
 import morgan from "morgan";
 import helmet from "helmet";
 import cors from "cors";
@@ -10,7 +14,6 @@ import api from "./routes";
 import userServiceRoutes from "./routes/ClientService.route";
 import commandeServiceRoutes from "./routes/CommandeService.route";
 import livraisonServiceRoutes from "./routes/LivraisonService.route";
-
 
 const app = express();
 
@@ -24,12 +27,38 @@ app.use("/gateway/client-service", userServiceRoutes);
 app.use("/gateway/commande-service", commandeServiceRoutes);
 app.use("/gateway/livraison-service", livraisonServiceRoutes);
 
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+  res.status(500).json({ message: err.message });
+});
+
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = createServer(app);
+export const io = new Server(server, {
+  cors: { origin: "http://localhost:5173" },
+});
+
+app.set("io", io);
+
+// app.listen(port, () => {
+//   console.log(
+//     `[server]:🗄️  Gateway Server is running at http://localhost:${port}`
+//   );
+// });
+
+io.on("connection", (socket) => {
+  console.log("🔌 New client connected");
+  socket.on("disconnect", () => {
+    console.log("❌ User disconnected");
+  });
+});
+
+server.listen(port, () => {
   console.log(
     `[server]:🗄️  Gateway Server is running at http://localhost:${port}`
   );
 });
 
-export default app;
+// export default app;
+export default server;
